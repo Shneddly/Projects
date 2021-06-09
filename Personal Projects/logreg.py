742012a5ca2131623c03c06a3b920fbf077a8534
Shneddly <83787460+Shneddly@users.noreply.github.com>
2021-06-08 21:54:08 -0700

    Create logreg.py

---
+++ Personal Projects/logreg.py
@@ -0,0 +1,32 @@
+'''
+Simple logistic regression algorithm
+'''
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def sigmoid(z):
+    # Sigmoid function = 1/(1+e^(-z))
+    return 1 / (1 + np.exp(-z))
+
+def gradient_descent(features, target, alpha, epochs):
+    
+    size = features.shape[0]
+    weights = np.zeros(features.shape[1])
+    bias = 0
+    parameters = {}
+    
+    for epoch in range(epochs):
+        
+        sigma = sigmoid(np.dot(features.T, weights) + bias)
+        cost = -1/size * np.sum(target * np.log(sigma) + (1 - target) * np.log(1 - sigma))
+        dW = 1/size * np.dot(features.T, (sigma - target))
+        dB = 1/size * np.sum(sigma - target)
+        weights -= alpha * dW
+        bias -= alpha * dB
+    
+    parameters['weights'] = weights
+    parameters['bias'] = bias
+    
+    return parameters
